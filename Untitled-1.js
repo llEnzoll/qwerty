0fc53d056b549e2203c39f0345cbd6ee27f59c5e
Эрик <erik@MacBook-Air-Erik.local>
2025-09-15 13:20:15 +0300

    Первый коммит

---
+++ Untitled-1.js
@@ -0,0 +1,140 @@
+// Задача 1
+console.log("=== Задача 1 ===");
+let songName = "I'm a barbie girl 'f'";
+console.log("Now playing: " + songName);
+
+// Задача 2
+console.log("\n=== Задача 2 ===");
+let catMood = "angry 'z'";
+console.log("Your cat is " + catMood);
+
+// Задача 3
+console.log("\n=== Задача 3 ===");
+let isImpostor = true;
+console.log("Red is impostor? " + isImpostor);
+
+// Задача 4
+console.log("\n=== Задача 4 ===");
+let wow = "wow";
+let soJS = "so JavaScript";
+let muchCode = "much code";
+console.log(wow + " " + soJS + " " + muchCode);
+
+// Задача 5
+console.log("\n=== Задача 5 ===");
+let likes = 77;
+let verified = false;
+console.log("likes type:", typeof likes);
+console.log("verified type:", typeof verified);
+
+// Задача 6
+console.log("\n=== Задача 6 ===");
+let isOnion = true;
+console.log("isOnion type:", typeof isOnion);
+
+// Задача 7
+console.log("\n=== Задача 7 ===");
+let year = 2077;
+let hero = "V";
+let isBuggy = true;
+console.log("year type:", typeof year);
+console.log("hero type:", typeof hero);
+console.log("isBuggy type:", typeof isBuggy);
+
+// Задача 8
+console.log("\n=== Задача 8 ===");
+let prize = 1000000;
+console.log("prize type:", typeof prize);
+
+// Задача 9
+console.log("\n=== Задача 9 ===");
+let situation = "fire";
+let reaction = "this is fine";
+console.log("situation type:", typeof situation);
+console.log("reaction type:", typeof reaction);
+
+// Задача 10
+console.log("\n=== Задача 10 ===");
+// Для консольной версии используем имитацию prompt
+let nickname = "Player123"; // В реальности было бы: prompt("What's your Minecraft nickname?")
+console.log("Welcome to the server, " + nickname + " ^");
+
+// Задача 11
+console.log("\n=== Задача 11 ===");
+let show = "Breaking Bad"; // В реальности было бы: prompt("What's your favorite TV show?")
+console.log("Tonight we binge: " + show + " {}");
+
+// Задача 12
+console.log("\n=== Задача 12 ===");
+let wantDocs = true; // В реальности было бы: confirm("Do you want a link to cool JS docs?")
+if (wantDocs) {
+    console.log("Never gonna give you up ^");
+}
+
+// Задача 13
+console.log("\n=== Задача 13 ===");
+let hours = 5; // В реальности было бы: prompt("How many hours did you scroll today?")
+console.log("Bruh, that's " + hours + " hours... touch grass ^");
+
+// Задача 14
+console.log("\n=== Задача 14 ===");
+let bitcoinPrice = "50000"; // В реальности было бы: prompt("Введите цену биткоина")
+let bitcoinNumber = Number(bitcoinPrice);
+console.log("Цена биткоина + 1000:", bitcoinNumber + 1000);
+
+// Задача 15
+console.log("\n=== Задача 15 ===");
+let n = "3"; // В реальности было бы: prompt("Введите число n")
+let nNumber = Number(n);
+console.log("Skibidi ".repeat(nNumber));
+
+// Задача 16
+console.log("\n=== Задача 16 ===");
+let coffees = "2"; // В реальности было бы: prompt("How many coffees today?")
+let coffeesNumber = Number(coffees);
+console.log("Total coffees:", coffeesNumber + 1);
+
+// Задача 17
+console.log("\n=== Задача 17 ===");
+let karma = "50"; // В реальности было бы: prompt("Введите значение karma")
+let karmaNumber = Number(karma);
+console.log("Total karma:", karmaNumber + 10);
+
+// Задача 18
+console.log("\n=== Задача 18 ===");
+let likesCount = "100"; // В реальности было бы: prompt("How many likes did your post get?")
+let likesNumber = Number(likesCount);
+console.log("Total likes:", likesNumber + 420);
+
+// Задача 19
+console.log("\n=== Задача 19 ===");
+let money = 1000;
+money += 250;
+console.log("Total money:", money);
+
+// Задача 20
+console.log("\n=== Задача 20 ===");
+let coins = 350;
+let boosters = 5;
+console.log("Total score:", coins + boosters * 100);
+
+// Задача 21
+console.log("\n=== Задача 21 ===");
+let pikachuHP = 100;
+let attack = 35;
+pikachuHP -= attack;
+console.log("Pikachu HP after attack:", pikachuHP);
+
+// Задача 22
+console.log("\n=== Задача 22 ===");
+let players = 456;
+let eliminated = 100;
+let remainingPlayers = players - eliminated;
+console.log("Players remaining:", remainingPlayers);
+
+// Задача 23
+console.log("\n=== Задача 23 ===");
+let a = 10;
+let b = 3;
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
